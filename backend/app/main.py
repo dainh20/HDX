6c75085c96445bd8f02ea736d3c204daf23a8188
dai <nguyenhdai2001@gmail.com>
2026-04-13 17:07:34 +0700

    Initial commit: HDX project

--- backend/app/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from app.domain.user.router import router as user_router
-
-app = FastAPI()
-app.include_router(user_router)
--- backend/migrations/001_init_schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE SCHEMA core;
-CREATE SCHEMA account;
-CREATE SCHEMA market;
-CREATE SCHEMA trading;
-CREATE SCHEMA portfolio;
-CREATE SCHEMA accounting;
